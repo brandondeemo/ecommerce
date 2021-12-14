1b51fcaf600b18f4280d8341ffe8263e2493fe16
brandondeemo <sun1206217507@gmail.com>
2021-12-14 03:18:47 -0600

    project initialization

---
+++ ecommerce-product/src/test/java/com/ecommerce/ecommerceproduct/EcommerceProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecommerceproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
